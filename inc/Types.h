aabdfbf48faee6535831f30d2f26b12665d2e1b0
Tyler Robbins <tyler@familyrobbins.com>
2019-05-05 18:25:12 -0700

    Added documentation.

--- inc/Types.h
+++ inc/Types.h
@@ -1,31 +1,47 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-#include <vector>
-#include <cstdint>
+#include <vector> // std::vector
+#include <cstdint> // uint32_t, uint8_t
 
 namespace MapNormalizer {
+    /**
+     * @brief A 2D point
+     */
     struct Point2D {
         uint32_t x;
         uint32_t y;
     };
 
+    /**
+     * @brief An RGB color value
+     */
     struct Color {
         uint8_t r;
         uint8_t g;
         uint8_t b;
     };
 
+    /**
+     * @brief A pixel, which is a point and color
+     */
     struct Pixel {
         Point2D point;
         Color color;
     };
 
+    /**
+     * @brief A polygon, which may be a solid color shape and a vector of all
+     *        pixels which make it up
+     */
     struct Polygon {
         std::vector<Pixel> pixels;
-        Color u_color; // unique color value
+        Color u_color; // TODO: This should be just the solid color fo this shape
     };
 
+    /**
+     * @brief A list of all shapes
+     */
     using PolygonList = std::vector<Polygon>;
 }
 
--- inc/UniqueColorGenerator.h
+++ inc/UniqueColorGenerator.h
@@ -1,7 +1,7 @@
 #ifndef UNIQUE_COLOR_GENERATOR_H
 #define UNIQUE_COLOR_GENERATOR_H
 
-#include "Types.h"
+#include "Types.h" // Color
 
 namespace MapNormalizer {
     Color generateUniqueColor(uint32_t);
