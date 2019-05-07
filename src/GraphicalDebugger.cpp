f78b22de7eb0326814e1f86430e66153013f5cc1
Tyler Robbins <tyler@familyrobbins.com>
2019-05-06 17:51:51 -0700

    Added code to enforce minimum and maximum width and height requirements on the window spawned by the graphical debugger. Added code which will enforce that the graphical worker thread will not sleep if the window is large enough.

--- src/GraphicalDebugger.cpp
+++ src/GraphicalDebugger.cpp
@@ -6,14 +6,28 @@
 #include <iostream> // std::cerr
 #include <thread> // std::this_thread
 
+#define NOMINMAX
+#include <cmath>
+
 #include "BitMap.h" // BitMap
 #include "ShapeFinder.h"
 
-#ifdef ENABLE_GRAPHICS 
+#ifdef ENABLE_GRAPHICS
 # include <SDL.h> // SDL_*
 #endif
 
 static std::mutex graphics_debug_mutex;
+
+// Maximum width + height that SDL will create a window for
+static const auto MAX_SCREEN_WIDTH = 16384;
+static const auto MAX_SCREEN_HEIGHT = 16384;
+
+// Minimum width + height that I want to allow
+static const auto MIN_SCREEN_WIDTH = 100;
+static const auto MIN_SCREEN_HEIGHT = 100;
+static const auto NUM_PIX_REQ_SLEEP = 10000;
+
+static bool should_sleep = true;
 
 /**
  * @brief Worker function which manages displaying graphical debugging data.
@@ -25,7 +39,7 @@ static std::mutex graphics_debug_mutex;
 void MapNormalizer::graphicsWorker(BitMap* image, unsigned char* disp_data,
                                    bool& done)
 {
-#ifndef ENABLE_GRAPHICS 
+#ifndef ENABLE_GRAPHICS
     (void)image;
     (void)disp_data;
     (void)done;
@@ -37,12 +51,35 @@ void MapNormalizer::graphicsWorker(BitMap* image, unsigned char* disp_data,
         return;
     }
 
+    SDL_DisplayMode current;
+    if(SDL_GetCurrentDisplayMode(0, &current) != 0) {
+        std::cerr << "Failed to get current display mode! " << SDL_GetError()
+                  << std::endl;
+        return;
+    }
+
+    // (8/10) of the current screen resolution is the maximum supported
+    const auto MAX_SCREEN_WIDTH = (8 * current.w) / 10;
+    const auto MAX_SCREEN_HEIGHT = (8 * current.h) / 10;
+
+    auto width = std::min(image->width, MAX_SCREEN_WIDTH);
+    auto height = std::min(image->height, MAX_SCREEN_HEIGHT);
+
+    if(width < MIN_SCREEN_WIDTH)
+        width *= 10;
+    if(height < MIN_SCREEN_HEIGHT)
+        height *= 10;
+
+    std::cout << "Creating a window of size (" << width << ',' << height << ")"
+              << std::endl;
+
+    ::should_sleep = (image->width * image->height) > NUM_PIX_REQ_SLEEP;
+
     // Create SDL window to write to
     SDL_Window* window = SDL_CreateWindow("Shape Finder Debugger",
                                           SDL_WINDOWPOS_UNDEFINED,
                                           SDL_WINDOWPOS_UNDEFINED,
-                                          image->width * 10,
-                                          image->height * 10,
+                                          width, height,
                                           SDL_WINDOW_SHOWN
                                           );
 
@@ -97,7 +134,7 @@ kill_graphics_worker:
 }
 
 /**
- * @brief Writes a color to the screen 
+ * @brief Writes a color to the screen
  *
  * @param debug_data The color data location to write into
  * @param w The width of the color data
@@ -118,7 +155,8 @@ void MapNormalizer::writeDebugColor(unsigned char* debug_data, uint32_t w,
     if(debug_data != nullptr) {
         using namespace std::chrono_literals;
 
-        std::this_thread::sleep_for(0.01s);
+        if(should_sleep)
+            std::this_thread::sleep_for(0.01s);
 
         uint32_t index = xyToIndex(w * 3, x * 3, y);
 
