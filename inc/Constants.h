a5f3e9bba9673880fc7f940baf65ed1efc3cd242
Tyler Robbins <tyler@familyrobbins.com>
2019-06-10 00:14:41 -0700

    Added a file to hold useful constants.

---
+++ inc/Constants.h
@@ -0,0 +1,14 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <cstdint>
+
+namespace MapNormalizer {
+    const std::uint32_t RED_MASK   = 0xFF0000;
+    const std::uint32_t BLUE_MASK  = 0x00FF00;
+    const std::uint32_t GREEN_MASK = 0x0000FF;
+    const std::uint32_t COLOR_MASK = 0xFFFFFF;
+}
+
+#endif
+
