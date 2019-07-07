04b5acf798d3faafb4ed00215d9aa889e0335560
Tyler Robbins <tyler@familyrobbins.com>
2019-07-07 02:14:28 -0700

    Added an Assembly file and header file to load each binary of color values generated by color_generator.py.

---
+++ inc/ColorArray.h
@@ -0,0 +1,19 @@
+#ifndef COLOR_ARRAY_H
+#define COLOR_ARRAY_H
+
+extern "C" {
+    extern const unsigned char MN_ALL_LANDS[];
+    extern const unsigned int  MN_ALL_LANDS_SIZE;
+
+    extern const unsigned char MN_ALL_SEAS[];
+    extern const unsigned int  MN_ALL_SEAS_SIZE;
+
+    extern const unsigned char MN_ALL_LAKES[];
+    extern const unsigned int  MN_ALL_LAKES_SIZE;
+
+    extern const unsigned char MN_ALL_UNKNOWNS[];
+    extern const unsigned int  MN_ALL_UNKNOWNS_SIZE;
+}
+
+#endif
+
