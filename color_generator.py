24e0acb8ea60a885a868f8517fc576ed037f95a3
Tyler Robbins <tyler@familyrobbins.com>
2019-07-12 17:23:00 -0700

    Rearranged code in color_generator so that common code is in functions. Also made it so that the script will only generate one file per call, depending on a given command line parameter.

--- color_generator.py
+++ color_generator.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
 
+import sys
 import random
 import colorsys
 from array import array
 
-RED_START = [255, 0, 0]
-GREEN_START = [0, 255, 0]
-BLUE_START = [0, 0, 255]
-
-land_colors = []
-sea_colors = []
-lake_colors = []
-unknown_colors = []
+colors = []
 
 # Maximum possible legal value for hue
 MAX_HUE_VALUE = 360
@@ -19,74 +13,48 @@ MAX_HUE_VALUE = 360
 MIN_VAL_VALUE = 20
 MIN_SAT_VALUE = 20
 
-print("Generating land colors...")
+def genColor(hue, sat, val):
+    return [int(255 * i) for i in colorsys.hsv_to_rgb(hue / MAX_HUE_VALUE,
+                                                      sat / 100.0,
+                                                      val / 100.0)]
 
-# Generate land colors (70 < saturation < 155)
-for hue in range(70, 155):
-    for sat in range(MIN_SAT_VALUE, 100):
-        for val in range(MIN_VAL_VALUE, 100):
-            color = [int(255 * i) for i in colorsys.hsv_to_rgb(hue / MAX_HUE_VALUE,
-                                                               sat / 100.0,
-                                                               val / 100.0)]
+def genAllColors(h_range, s_range, v_range):
+    for hue in range(*h_range):
+        for sat in range(*s_range):
+            for val in range(*v_range):
+                color = genColor(hue, sat, val)
+                colors.append(array('B', color))
 
-            land_colors.append(array('B', color))
+if "lands" in sys.argv:
+    filename = "lands.bin"
+    # Land colors (70 < saturation < 155)
+    hue_range = (70, 155)
+elif "seas" in sys.argv:
+    filename = "seas.bin"
+    # Sea colors (175 < saturation < 255)
+    hue_range = (175, 255)
+elif "lakes" in sys.argv:
+    filename = "lakes.bin"
+    # Lake colors (256 < saturation < 335)
+    hue_range = (256, 335)
+elif "unknowns" in sys.argv:
+    filename = "unknowns.bin"
+    # All unknown colors (0 < saturation < 69
+    hue_range = (0, 69)
+else:
+    print("Missing parameter.", file=sys.stderr)
+    exit(1)
 
-print("Generating sea colors...")
-# Generate sea colors (175 < saturation < 255)
-for hue in range(175, 255):
-    for sat in range(MIN_SAT_VALUE, 100):
-        for val in range(MIN_VAL_VALUE, 100):
-            color = [int(255 * i) for i in colorsys.hsv_to_rgb(hue / MAX_HUE_VALUE,
-                                                               sat / 100.0,
-                                                               val / 100.0)]
+print(f"Generating {filename[:4]} colors...")
+genAllColors(hue_range, (MIN_SAT_VALUE, 100), (MIN_VAL_VALUE, 100))
 
-            sea_colors.append(array('B', color))
+print(f"Generated {len(colors)} colors.")
 
-print("Generating lake colors...")
-# Generate lake colors (256 < saturation < 335)
-for hue in range(256, 335):
-    for sat in range(MIN_SAT_VALUE, 100):
-        for val in range(MIN_VAL_VALUE, 100):
-            color = [int(255 * i) for i in colorsys.hsv_to_rgb(hue / MAX_HUE_VALUE,
-                                                               sat / 100.0,
-                                                               val / 100.0)]
+print("Shuffling...")
+random.shuffle(colors)
 
-            lake_colors.append(array('B', color))
+colors = b"".join(colors)
 
-print("Generating unknown colors...")
-# Generate all unknown colors (0 < saturation < 69
-for hue in range(0, 69):
-    for sat in range(MIN_SAT_VALUE, 100):
-        for val in range(MIN_VAL_VALUE, 100):
-            color = [int(255 * i) for i in colorsys.hsv_to_rgb(hue / MAX_HUE_VALUE,
-                                                               sat / 100.0,
-                                                               val / 100.0)]
-
-            unknown_colors.append(array('B', color))
-
-print(f"Generated {len(land_colors)} land colors.")
-print(f"Generated {len(sea_colors)} sea colors.")
-print(f"Generated {len(lake_colors)} lake colors.")
-print(f"Generated {len(unknown_colors)} unknown colors.")
-
-print("Shuffling all color lists...")
-random.shuffle(land_colors)
-random.shuffle(sea_colors)
-random.shuffle(lake_colors)
-random.shuffle(unknown_colors)
-
-land_colors = b"".join(land_colors)
-sea_colors = b"".join(sea_colors)
-lake_colors = b"".join(lake_colors)
-unknown_colors = b"".join(unknown_colors)
-
-land_file = open("lands.bin", "wb")
-sea_file = open("seas.bin", "wb")
-lake_file = open("lakes.bin", "wb")
-unknown_file = open("unknowns.bin", "wb")
-
-land_file.write(land_colors)
-sea_file.write(sea_colors)
-lake_file.write(lake_colors)
-unknown_file.write(unknown_colors)
+file = open(filename, "wb")
+file.write(colors)
 
