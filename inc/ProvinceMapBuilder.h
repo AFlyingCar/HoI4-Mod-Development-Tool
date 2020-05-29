f2fea1734600641a4a4359c719b58d33cc4264f1
Tyler Robbins <tyler@familyrobbins.com>
2020-05-29 00:20:43 -0700

    Reworked how province information would be stored in color values. Replaced old MASK constants with an enum containing the new bit masks. getProvinceType now returns just the ProvinceType rather than a pair of the province type and the mask. All getters no longer take a mask. Added a getState() function.

--- inc/ProvinceMapBuilder.h
+++ inc/ProvinceMapBuilder.h
@@ -3,21 +3,46 @@
 
 #include "Types.h"
 
-namespace MapNormalizer {
-    const auto PROV_LAND_MASK = 0xFF0000;
-    const auto PROV_LAKE_MASK = 0x00FF00;
-    const auto PROV_SEA_MASK  = 0x0000FF;
 
-    const auto TERRAIN_MASK      = 0b11110000;
-    const auto IS_COASTAL_MASK   = 0b00001000;
-    const auto CONTINENT_ID_MASK = 0x00000111; // Maximum of 8 total continents
+/*
+ * Province data is a 24-bit color value
+ *
+ *   6 bits       1 bit        3 bits      2 bits   12 bits
+ * +---------+------------+--------------+--------+----------+
+ * | Terrain | Is Coastal | Continent ID |  Type  | State ID |
+ * +---------+------------+--------------+--------+----------+
+ *
+ */
+
+namespace MapNormalizer {
+    /**
+     * @brief Masking values to get province data
+     */
+    enum ProvinceMasks: std::uint32_t {
+        // Max of 64 types of terrain
+        PROV_TERRAIN_MASK = 0xFC0000,
+
+        // Boolean value
+        PROV_COASTAL_MASK = 0x20000,
+
+        // Max of 8 different continents
+        PROV_CONTINENT_ID_MASK = 0x1C000,
+
+        // Mask for getting what type of province this is
+        // Valid values are UNKNOWN (0), LAND (1), LAKE (2), SEA (3)
+        PROV_TYPE_MASK = 0x3000,
+
+        // Max of 4096 different provinces
+        PROV_STATE_ID_MASK = 0xFFF
+    };
 
     ProvinceList createProvinceList(const PolygonList&);
-    std::pair<ProvinceType, std::uint32_t> getProvinceType(std::uint32_t);
-    std::pair<ProvinceType, std::uint32_t> getProvinceType(const Color&);
-    bool isCoastal(const Color&, std::uint32_t);
-    Terrain getTerrainType(const Color&, std::uint32_t);
-    size_t getContinent(const Color&, std::uint32_t);
+    ProvinceType getProvinceType(std::uint32_t);
+    ProvinceType getProvinceType(const Color&);
+    bool isCoastal(const Color&);
+    Terrain getTerrainType(const Color&);
+    Continent getContinent(const Color&);
+    State getState(const Color&);
 }
 
 #include <fstream>
