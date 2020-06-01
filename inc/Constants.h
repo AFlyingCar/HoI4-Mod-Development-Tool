69e37d207250c4353756ecd6d48b9e81a3ed612b
Tyler Robbins <tyler@familyrobbins.com>
2020-05-31 23:25:40 -0700

    Swapped values of BLUE_MASK and GREEN_MASK back to normal.

--- inc/Constants.h
+++ inc/Constants.h
@@ -6,8 +6,8 @@
 
 namespace MapNormalizer {
     const std::uint32_t RED_MASK   = 0xFF0000;
-    const std::uint32_t BLUE_MASK  = 0x00FF00;
-    const std::uint32_t GREEN_MASK = 0x0000FF;
+    const std::uint32_t GREEN_MASK = 0x00FF00;
+    const std::uint32_t BLUE_MASK  = 0x0000FF;
     const std::uint32_t COLOR_MASK = 0xFFFFFF;
 
     const std::uint16_t BM_TYPE = 19778; // BM
