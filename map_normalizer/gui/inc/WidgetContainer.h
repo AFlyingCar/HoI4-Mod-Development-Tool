9b205c6336394e214734742266145f3b65c7aa65
Tyler Robbins <tyler@familyrobbins.com>
2021-04-26 20:07:46 -0700

    Simplified WidgetContainer to simply mark that each newly created W is managed by the parent widget, rather than by the WidgetContainer itself. Added documentation. Removed the now unneeded WidgetContainer.cpp file.

--- map_normalizer/gui/inc/WidgetContainer.h
+++ map_normalizer/gui/inc/WidgetContainer.h
@@ -1,31 +1,41 @@
 #ifndef WIDGET_CONTAINER_H
 # define WIDGET_CONTAINER_H
 
-# include <vector>
-
 # include "gtkmm/widget.h"
 
 namespace MapNormalizer::GUI {
+    /**
+     * @brief A generic container for widgets, provides a simplified interface
+     *        for adding widgets to a parent container
+     */
     class WidgetContainer {
         public:
             WidgetContainer() = default;
-            virtual ~WidgetContainer();
+            virtual ~WidgetContainer() = default;
 
         protected:
+            /**
+             * @brief Adds a new widget of type W to the parent widget
+             * @par Note that new W objects are created as "managed", meaning
+             *      that their lifetime is controlled by the parent they get
+             *      added to.
+             *
+             * @tparam W The type of widget to add
+             * @tparam Args The type of every argument to create a new W
+             * @param args Every argument to get forwarded into a call to `new W{}`
+             *
+             * @return the newly created W.
+             */
             template<typename W, typename... Args>
             W* addWidget(Args&&... args) {
-                // TODO: Mark that W is managed by the parent container instead?
-                m_widgets.push_back(new W{std::forward<Args>(args)...});
+                W* widget = manage(new W{std::forward<Args>(args)...});
 
-                addWidgetToParent(*m_widgets.back());
+                addWidgetToParent(*widget);
 
-                return static_cast<W*>(m_widgets.back());
+                return widget;
             }
 
             virtual void addWidgetToParent(Gtk::Widget&) = 0;
-
-        private:
-            std::vector<Gtk::Widget*> m_widgets;
     };
 }
 
--- map_normalizer/gui/src/WidgetContainer.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "WidgetContainer.h"
-
-MapNormalizer::GUI::WidgetContainer::~WidgetContainer() {
-    for(auto&& widget : m_widgets) {
-        delete widget;
-    }
-}
-
