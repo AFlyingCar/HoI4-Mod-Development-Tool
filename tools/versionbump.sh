c52a120b8562e34a73e3391d4d255b75d6591127
Tyler Robbins <tyler@familyrobbins.com>
2021-05-23 00:18:40 -0700

    Fixed version bump script.

--- tools/versionbump.sh
+++ tools/versionbump.sh
@@ -1,5 +1,3 @@
 
-version=$(cat "$1/.config/version.txt")
-
-echo $(awk -v versionDiff="$2" -F. -f "$1/tools/versionbump.awk" OFS=. <<< "$version") > "$1/.config/version.txt"
+echo $(awk -v versionDiff="$2" -F. -f "$1/tools/versionbump.awk" OFS=. "$1/.config/version.txt") > "$1/.config/version.txt"
 
