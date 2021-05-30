ad6c7e570db75b3cb1142e88db32e38b7635496e
Tyler Robbins <tyler@familyrobbins.com>
2021-05-29 20:11:27 -0700

    Added helper function to get the path that the unit test executable is in.

--- tests/inc/TestUtils.h
+++ tests/inc/TestUtils.h
@@ -1,12 +1,16 @@
 #ifndef TEST_UTILS_H
 # define TEST_UTILS_H
 
+# include <filesystem>
+
 # define STR(X) #X
 # define PRIM_CONCAT(A,B) A##B
 # define CONCAT(A,B) PRIM_CONCAT(A, B)
 
 namespace MapNormalizer::UnitTests {
     bool useVerboseOutput();
+
+    std::filesystem::path getTestProgramPath();
 }
 
 #endif
--- tests/src/TestUtils.cpp
+++ tests/src/TestUtils.cpp
@@ -3,6 +3,14 @@
 
 #include <cstdlib>
 
+#ifdef _WIN32
+# include "windows.h"
+# define PATH_MAX FILENAME_MAX
+#else
+# include <unistd.h>
+# include <linux/limits.h>
+#endif
+
 #define ENVVAR_PREFIX ENVVAR_
 
 #define DEF_ENVVAR_IMPL(VARNAME) \
@@ -15,5 +23,16 @@ DEF_ENVVAR(VERBOSE);
 
 bool MapNormalizer::UnitTests::useVerboseOutput() {
     return std::getenv(ENVVAR_VERBOSE) != nullptr;
+}
+
+std::filesystem::path MapNormalizer::UnitTests::getTestProgramPath() {
+    char path[PATH_MAX];
+#ifdef _WIN32
+    GetModuleFileName(NULL, path, PATH_MAX);
+#else
+    readlink("/proc/self/exe", path, PATH_MAX);
+#endif
+
+    return std::filesystem::path(path).parent_path();
 }
 
