1b4b10be8fd4580d03cadda58bac330c6f048352
Tyler Robbins <tyler@familyrobbins.com>
2021-06-03 17:56:22 -0700

    Added --noconfirm option.

--- win32.bootstrap.sh
+++ win32.bootstrap.sh
@@ -1,8 +1,8 @@
 
-pacman -S mingw-w64-x86_64-clang
-pacman -S mingw-w64-x86_64-gtkmm3
+pacman -S --noconfirm mingw-w64-x86_64-clang
+pacman -S --noconfirm mingw-w64-x86_64-gtkmm3
 
-pacman -S pkg-config
+pacman -S --noconfirm pkg-config
 
 # TODO: Run pkg-config here and parse the output with CMake?
 
