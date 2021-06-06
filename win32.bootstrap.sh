a5ac2507099bb34dbe29c30abb0f4a2328cac697
Tyler Robbins <tyler@familyrobbins.com>
2021-06-05 23:11:53 -0700

    Removed unecessary packages from bootstrap.

--- win32.bootstrap.sh
+++ win32.bootstrap.sh
@@ -1,5 +1,5 @@
 
-pacman -S --noconfirm mingw-w64-x86_64-clang mingw-w64-x86_64-gtkmm3 mingw-w64-x86_64-cmake pkg-config
+pacman -S --noconfirm mingw-w64-x86_64-gtkmm3 mingw-w64-x86_64-cmake pkg-config
 
 # TODO: Run pkg-config here and parse the output with CMake?
 
