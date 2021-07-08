6f47c865e384a997f2795f62695370e9b26c5dc9
Tyler Robbins <tyler@familyrobbins.com>
2021-07-07 23:21:52 -0700

    Added code to write logs to a file.

--- map_normalizer/exe/src/main.cpp
+++ map_normalizer/exe/src/main.cpp
@@ -5,9 +5,24 @@
  */
 
 #include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <queue>
+#include <cerrno> // errno
+#include <cstring> // strerror
+
+#ifdef WIN32
+# include <shlwapi.h>
+# pragma comment(lib,"shlwapi.lib")
+# include "shlobj.h"
+#else
+# include <pwd.h>
+# include <unistd.h>
+#endif
 
 #include "ArgParser.h"
 #include "Options.h"
+#include "Constants.h"
 
 #include "Logger.h"
 #include "ConsoleOutputFunctions.h"
@@ -15,6 +30,38 @@
 #include "Interfaces.h"
 
 MapNormalizer::ProgramOptions MapNormalizer::prog_opts;
+
+/**
+ * @brief Gets the path to the file where logs should get written to
+ *
+ * @return 
+ */
+std::filesystem::path getLogOutputFilePath() {
+    std::filesystem::path log_path;
+#ifdef WIN32
+    TCHAR path[MAX_PATH];
+    if(SUCCEEDED(SHGetFolderPath(NULL, CSIDL_LOCAL_APPDATA, NULL, 0, path)))
+    {
+        log_path = path;
+    }
+
+    log_path = log_path / MapNormalizer::APPLICATION_SIMPLE_NAME;
+#else
+    static struct passwd* user_pw = getpwuid(getuid());
+    char* home_dir = user_pw->pw_dir;
+
+    log_path = home_dir;
+    log_path = log_path / ".local" / MapNormalizer::APPLICATION_SIMPLE_NAME;
+#endif
+
+    if(!std::filesystem::exists(log_path)) {
+        std::filesystem::create_directory(log_path);
+    }
+
+    log_path = log_path / (MapNormalizer::APPLICATION_SIMPLE_NAME + MapNormalizer::LOG_FILE_EXTENSION);
+
+    return log_path;
+}
 
 /**
  * @brief The starting point of the program.
@@ -55,12 +102,57 @@ int main(int argc, char** argv) {
             return MapNormalizer::Log::outputWithFormatting(message);
         });
 
+    std::shared_ptr<std::ofstream> log_output_file(nullptr);
+    bool disable_file_log_output = false;
+
+    MapNormalizer::Log::Logger::registerOutputFunction(
+        [log_output_file, &disable_file_log_output](const MapNormalizer::Log::Message& message) -> bool
+        {
+            if(disable_file_log_output) return true;
+
+            static std::queue<MapNormalizer::Log::Message> messages;
+
+            messages.push(message);
+
+            if(!log_output_file) return true;
+
+            while(!messages.empty()) {
+                auto&& message = messages.front();
+                if(!MapNormalizer::Log::outputToStream(message, false, true, 
+                    [&log_output_file](uint8_t) -> std::ostream& {
+                        return *log_output_file;
+                    }, true))
+                {
+                    return false;
+                }
+                messages.pop();
+            }
+
+            return true;
+        }
+    );
+
     // Parse the command-line arguments
     MapNormalizer::prog_opts = MapNormalizer::parseArgs(argc, argv);
+
+    if(!MapNormalizer::prog_opts.dont_write_logfiles) {
+        auto log_output_path = getLogOutputFilePath();
+        std::ofstream file(log_output_path);
+        if(!file) {
+            WRITE_ERROR("Failed to open ", log_output_path, ". Reason: ", strerror(errno));
+            disable_file_log_output = true;
+        } else {
+            log_output_file.reset(new std::ofstream(std::move(file)));
+            WRITE_STDOUT("Log files will get written to ", log_output_path);
+        }
+    } else {
+        disable_file_log_output = true;
+    }
 
     // Figure out if we should stop now based on the status of the parsing
     switch(MapNormalizer::prog_opts.status) {
         case 1:
+            WRITE_ERROR("Failed to parse program options. Exiting now.");
             return 1;
         case 2:
             return 0;
