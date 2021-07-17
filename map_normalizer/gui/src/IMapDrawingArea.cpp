f3ac585708e7683be2335c703b2cba5a49a6f6e6
Tyler Robbins <tyler@familyrobbins.com>
2021-07-17 12:44:01 -0700

    Moved most of the contents of MapDrawingArea and ProvincePreviewDrawingArea into interfaces.

---
+++ map_normalizer/gui/src/IMapDrawingArea.cpp
@@ -0,0 +1,151 @@
+
+#include "IMapDrawingArea.h"
+
+#include "Logger.h"
+#include "Constants.h"
+
+#include "GraphicalDebugger.h"
+#include "Util.h"
+
+MapNormalizer::GUI::IMapDrawingAreaBase::IMapDrawingAreaBase():
+    m_graphics_data(nullptr),
+    m_image(nullptr),
+    m_on_select([](auto...) { }),      // The default callback does nothing
+    m_on_multiselect([](auto...) { }),
+    m_scale_factor(DEFAULT_ZOOM)
+{ }
+
+
+bool MapNormalizer::GUI::IMapDrawingAreaBase::hasData() const {
+    return m_graphics_data != nullptr && m_image != nullptr;
+}
+
+void MapNormalizer::GUI::IMapDrawingAreaBase::setGraphicsData(const unsigned char* data)
+{
+    m_graphics_data = data;
+}
+
+void MapNormalizer::GUI::IMapDrawingAreaBase::setImage(const BitMap* image) {
+    m_image = image;
+}
+
+const unsigned char* MapNormalizer::GUI::IMapDrawingAreaBase::getGraphicsData() const
+{
+    return m_graphics_data;
+}
+
+auto MapNormalizer::GUI::IMapDrawingAreaBase::getImage() const -> const BitMap*
+{
+    return m_image;
+}
+
+void MapNormalizer::GUI::IMapDrawingAreaBase::setData(const BitMap* image,
+                                                      const unsigned char* data)
+{
+    setImage(image);
+    setGraphicsData(data);
+
+    resetZoom();
+}
+
+void MapNormalizer::GUI::IMapDrawingAreaBase::setOnProvinceSelectCallback(const SelectionCallback& callback)
+{
+    m_on_select = callback;
+}
+
+void MapNormalizer::GUI::IMapDrawingAreaBase::setOnMultiProvinceSelectionCallback(const SelectionCallback& callback)
+{
+    m_on_multiselect = callback;
+}
+
+void MapNormalizer::GUI::IMapDrawingAreaBase::setSelection() {
+    m_selection = std::nullopt;
+}
+
+void MapNormalizer::GUI::IMapDrawingAreaBase::setSelection(const SelectionInfo& selection)
+{
+    m_selection = selection;
+}
+
+void MapNormalizer::GUI::IMapDrawingAreaBase::zoom(ZoomDirection direction) {
+    switch(direction) {
+        case ZoomDirection::IN:
+            zoom(ZOOM_FACTOR);
+            break;
+        case ZoomDirection::OUT:
+            zoom(-ZOOM_FACTOR);
+            break;
+        case ZoomDirection::RESET:
+            resetZoom();
+            break;
+    }
+}
+
+void MapNormalizer::GUI::IMapDrawingAreaBase::zoom(double scale_factor_delta) {
+    m_scale_factor += scale_factor_delta;
+
+    onZoom();
+}
+
+/**
+ * @brief Will reset the zoom level
+ */
+void MapNormalizer::GUI::IMapDrawingAreaBase::resetZoom() {
+    // Do nothing if there is no data loaded
+    if(!hasData()) return;
+
+    auto* parent = getParent();
+
+    if(parent == nullptr) {
+        writeWarning("MapDrawingArea has no parent, setting zoom to ", DEFAULT_ZOOM);
+
+        m_scale_factor = DEFAULT_ZOOM;
+    } else {
+        double pwidth = parent->get_width();
+        double pheight = parent->get_height();
+
+        double iheight = m_image->info_header.height;
+
+        // Scale to the smallest dimension of the parent window
+        // But only scale down if the image is too large, don't worry about
+        //  trying to scale up a smaller image
+        if(pheight <= pwidth && pheight < iheight) {
+            m_scale_factor = (pheight / iheight);
+        } else {
+            m_scale_factor = DEFAULT_ZOOM;
+        }
+    }
+
+    writeDebug("Reset zoom to ", m_scale_factor);
+
+    onZoom();
+}
+
+double MapNormalizer::GUI::IMapDrawingAreaBase::getScaleFactor() const {
+    return m_scale_factor;
+}
+
+auto MapNormalizer::GUI::IMapDrawingAreaBase::getOnSelect() const
+    -> const SelectionCallback&
+{
+    return m_on_select;
+}
+
+auto MapNormalizer::GUI::IMapDrawingAreaBase::getOnMultiSelect() const
+    -> const SelectionCallback&
+{
+    return m_on_multiselect;
+}
+
+auto MapNormalizer::GUI::IMapDrawingAreaBase::getSelection() const
+    -> const std::optional<SelectionInfo>&
+{
+    return m_selection;
+}
+
+auto MapNormalizer::GUI::IMapDrawingAreaBase::getSelection()
+    -> std::optional<SelectionInfo>& 
+{
+    return m_selection;
+}
+
--- map_normalizer/gui/src/MapDrawingArea.cpp
+++ map_normalizer/gui/src/MapDrawingArea.cpp
@@ -12,21 +12,6 @@
 #include "GraphicalDebugger.h"
 #include "Util.h"
 
-MapNormalizer::GUI::MapDrawingArea::MapDrawingArea():
-    m_graphics_data(nullptr),
-    m_image(nullptr),
-    m_on_select([](auto...) { }),      // The default callback does nothing
-    m_on_multiselect([](auto...) { }),
-    m_scale_factor(DEFAULT_ZOOM)
-{
-    // Mark that we want to receive button presses
-    add_events(Gdk::BUTTON_PRESS_MASK);
-}
-
-bool MapNormalizer::GUI::MapDrawingArea::hasData() const {
-    return m_graphics_data != nullptr && m_image != nullptr;
-}
-
 bool MapNormalizer::GUI::MapDrawingArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
 {
     // Do nothing if we have no graphics data to actually render
@@ -36,11 +21,11 @@ bool MapNormalizer::GUI::MapDrawingArea::on_draw(const Cairo::RefPtr<Cairo::Cont
 
     // If a province is selected, then go ahead and draw the province preview
     //  on top of the map again
-    if(m_selection) {
-        auto&& [width, height] = calcDims(m_selection->bounding_box);
+    if(getSelection()) {
+        auto&& [width, height] = calcDims(getSelection()->bounding_box);
 
         auto stride = Cairo::ImageSurface::format_stride_for_width(Cairo::FORMAT_ARGB32, width);
-        auto province_image = Cairo::ImageSurface::create(m_selection->data.get(),
+        auto province_image = Cairo::ImageSurface::create(getSelection()->data.get(),
                                                           Cairo::FORMAT_ARGB32,
                                                           width, height,
                                                           stride);
@@ -69,12 +54,12 @@ bool MapNormalizer::GUI::MapDrawingArea::on_draw(const Cairo::RefPtr<Cairo::Cont
         province_cr->set_operator(Cairo::OPERATOR_SOURCE); // Reset the operator
 
         // Paint the province onto the full image
-        full_cr->set_source(province_image, m_selection->bounding_box.bottom_left.x, m_selection->bounding_box.top_right.y);
+        full_cr->set_source(province_image, getSelection()->bounding_box.bottom_left.x, getSelection()->bounding_box.top_right.y);
 #else
-        auto posx = m_selection->bounding_box.bottom_left.x;
-        auto posy = m_selection->bounding_box.top_right.y;
+        auto posx = getSelection()->bounding_box.bottom_left.x;
+        auto posy = getSelection()->bounding_box.top_right.y;
 
-        full_cr->scale(m_scale_factor, m_scale_factor);
+        full_cr->scale(getScaleFactor(), getScaleFactor());
         full_cr->set_source(province_image, posx, posy);
 #endif
         full_cr->paint();
@@ -94,45 +79,25 @@ bool MapNormalizer::GUI::MapDrawingArea::on_draw(const Cairo::RefPtr<Cairo::Cont
     return true;
 }
 
-bool MapNormalizer::GUI::MapDrawingArea::on_button_press_event(GdkEventButton* event)
-{
-    if(!hasData()) {
-        return true;
+/**
+ * @brief Scales the cached Pixbuf up or down based on the current scale factor
+ */
+void MapNormalizer::GUI::MapDrawingArea::rebuildImageCache() {
+    if(hasData()) {
+        auto iwidth = getImage()->info_header.width;
+        auto iheight = getImage()->info_header.height;
+
+        auto siwidth = iwidth * getScaleFactor();
+        auto siheight = iheight * getScaleFactor();
+
+        m_image_pixbuf = Gdk::Pixbuf::create_from_data(getGraphicsData(), Gdk::Colorspace::COLORSPACE_RGB, false, 8, iwidth, iheight, iwidth * 3);
+
+        m_image_pixbuf = m_image_pixbuf->scale_simple(siwidth, siheight, Gdk::INTERP_BILINEAR);
+
+        set_size_request(siwidth, siheight);
+    } else {
+        m_image_pixbuf.reset();
     }
-
-    // Is it a left-click?
-    if(event->type == GDK_BUTTON_PRESS && event->button == 1) {
-        // Note that x and y will be the values after scaling. If we want the
-        //  true coordinates, we have to invert the scaling
-        auto x = event->x * (1 / m_scale_factor);
-        auto y = event->y * (1 / m_scale_factor);
-
-        if(event->state & GDK_SHIFT_MASK) {
-            m_on_multiselect(x, y);
-        } else {
-            m_on_select(x, y);
-        }
-    }
-
-    return true;
-}
-
-void MapNormalizer::GUI::MapDrawingArea::setGraphicsData(const unsigned char* data)
-{
-    m_graphics_data = data;
-}
-
-void MapNormalizer::GUI::MapDrawingArea::setImage(const BitMap* image) {
-    m_image = image;
-}
-
-void MapNormalizer::GUI::MapDrawingArea::setData(const BitMap* image,
-                                                 const unsigned char* data)
-{
-    setImage(image);
-    setGraphicsData(data);
-
-    resetZoom();
 }
 
 void MapNormalizer::GUI::MapDrawingArea::graphicsUpdateCallback(const Rectangle& rectangle)
@@ -144,42 +109,8 @@ void MapNormalizer::GUI::MapDrawingArea::graphicsUpdateCallback(const Rectangle&
     queue_draw_area(rectangle.x, rectangle.y, rectangle.w, rectangle.h);
 }
 
-void MapNormalizer::GUI::MapDrawingArea::setOnProvinceSelectCallback(const SelectionCallback& callback)
-{
-    m_on_select = callback;
-}
-
-void MapNormalizer::GUI::MapDrawingArea::setOnMultiProvinceSelectionCallback(const SelectionCallback& callback)
-{
-    m_on_multiselect = callback;
-}
-
-void MapNormalizer::GUI::MapDrawingArea::setSelection() {
-    m_selection = std::nullopt;
-}
-
-void MapNormalizer::GUI::MapDrawingArea::setSelection(const SelectionInfo& selection)
-{
-    m_selection = selection;
-}
-
-void MapNormalizer::GUI::MapDrawingArea::zoom(ZoomDirection direction) {
-    switch(direction) {
-        case ZoomDirection::IN:
-            zoom(ZOOM_FACTOR);
-            break;
-        case ZoomDirection::OUT:
-            zoom(-ZOOM_FACTOR);
-            break;
-        case ZoomDirection::RESET:
-            resetZoom();
-            break;
-    }
-}
-
-void MapNormalizer::GUI::MapDrawingArea::zoom(double scale_factor_delta) {
-    m_scale_factor += scale_factor_delta;
-
+void MapNormalizer::GUI::MapDrawingArea::onZoom() {
+    // Rebuild the cached image
     rebuildImageCache();
 
     // We need to redraw the entire map if we zoom in/out
@@ -242,4 +173,6 @@ void MapNormalizer::GUI::MapDrawingArea::rebuildImageCache() {
         m_image_pixbuf.reset();
     }
 }
+
+void MapNormalizer::GUI::MapDrawingArea::init() { }
 
