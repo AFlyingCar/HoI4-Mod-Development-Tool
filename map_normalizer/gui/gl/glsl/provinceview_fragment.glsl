796101ccc78a4fc73f9dbd73d2aaa266f6d1a81b
Tyler Robbins <tyler@familyrobbins.com>
2021-07-23 22:02:55 -0700

    Fixed errors in glsl shaders.

--- map_normalizer/gui/gl/glsl/provinceview_fragment.glsl
+++ map_normalizer/gui/gl/glsl/provinceview_fragment.glsl
@@ -1,11 +1,12 @@
+#version 410 core
 
-#version 410
+out vec4 FragColor; // Output color value
 
 uniform sampler2D map_texture;
 
-in vec2 texture_coords;
+in vec2 texture_coords; // Input from vertex shader
 
 void main() {
-    FragColor = texture(map_texture, texture_coords);
+    FragColor = vec4(texture_coords, 0, 1); // texture(map_texture, texture_coords);
 }
 
