d33957d8be5c282337cd00973323720015bb82f5
Tyler Robbins <tyler@familyrobbins.com>
2021-08-17 01:58:31 -0700

    Updated provinceview shaders.

--- map_normalizer/gui/gl/glsl/provinceview_fragment.glsl
+++ map_normalizer/gui/gl/glsl/provinceview_fragment.glsl
@@ -7,6 +7,6 @@ uniform sampler2D map_texture;
 in vec2 texture_coords; // Input from vertex shader
 
 void main() {
-    FragColor = vec4(texture_coords, 0, 1); // texture(map_texture, texture_coords);
+    FragColor = texture(map_texture, texture_coords);
 }
 
--- map_normalizer/gui/gl/glsl/provinceview_vertex.glsl
+++ map_normalizer/gui/gl/glsl/provinceview_vertex.glsl
@@ -2,12 +2,13 @@
 
 layout(location=0) in vec4 position;
 
+uniform mat4 projection;
 uniform mat4 transform;
 
 out vec2 texture_coords;
 
 void main() {
     texture_coords = position.zw;
-    gl_Position = transform * vec4(position.xy, 0, 1);
+    gl_Position = projection * transform * vec4(position.xy, 0, 1);
 }
 
