1a452c064e281a9e58981533d7254b0ab1c1e063
Tyler Robbins <tyler@familyrobbins.com>
2021-08-18 23:14:09 -0700

    Added a class to handle representing the various copies of the map data. This is to replace the raw 'unsigned char*' values that were getting passed around, allowing them to instead be stored in a single object using smart pointers.

---
+++ map_normalizer/common/src/MapData.cpp
@@ -0,0 +1,59 @@
+#include "MapData.h"
+
+#include "Util.h"
+
+MapNormalizer::MapData::MapData(uint32_t width, uint32_t height):
+    m_width(width),
+    m_height(height),
+    m_input(new uint8_t[width * height * 3]),
+    m_provinces(new uint8_t[width * height * 3]),
+    m_province_outlines(new uint8_t[width * height])
+{
+}
+
+void MapNormalizer::MapData::close() {
+    m_closed = true;
+}
+
+uint32_t MapNormalizer::MapData::getWidth() const {
+    return m_width;
+}
+
+uint32_t MapNormalizer::MapData::getHeight() const {
+    return m_height;
+}
+
+std::pair<uint32_t, uint32_t> MapNormalizer::MapData::getDimensions() const {
+    return std::make_pair(m_width, m_height);
+}
+
+bool MapNormalizer::MapData::isClosed() const {
+    return m_closed;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+auto MapNormalizer::MapData::getInput() -> MapType {
+    return m_input;
+}
+
+auto MapNormalizer::MapData::getInput() const -> ConstMapType {
+    return m_input;
+}
+
+auto MapNormalizer::MapData::getProvinces() -> MapType {
+    return m_provinces;
+}
+
+auto MapNormalizer::MapData::getProvinces() const -> ConstMapType {
+    return m_provinces;
+}
+
+auto MapNormalizer::MapData::getProvinceOutlines() -> MapType {
+    return m_province_outlines;
+}
+
+auto MapNormalizer::MapData::getProvinceOutlines() const -> ConstMapType {
+    return m_province_outlines;
+}
+
