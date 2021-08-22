e80f4a86887658853ae7d730fb5480a96d3e8aa5
Tyler Robbins <tyler@familyrobbins.com>
2021-08-22 15:47:39 -0700

    Added an optional overridable handler for when the selection gets changed. Added code so that the owning GL::MapDrawingArea can be set on the object and can be referenced by subclasses.

--- map_normalizer/gui/gl/inc/IRenderingView.h
+++ map_normalizer/gui/gl/inc/IRenderingView.h
@@ -3,11 +3,14 @@
 
 # include <memory>
 # include <vector>
+# include <optional>
 
 # include "MapData.h"
+# include "IMapDrawingArea.h" // SelectionInfo
 
 namespace MapNormalizer::GUI::GL {
     class Program;
+    class MapDrawingArea;
 
     /**
      * @brief Base interface for implementing a rendering view
@@ -26,12 +29,23 @@ namespace MapNormalizer::GUI::GL {
             virtual void init() = 0;
 
             virtual void onMapDataChanged(std::shared_ptr<const MapData>) = 0;
+            virtual void onSelectionChanged(std::optional<IMapDrawingAreaBase::SelectionInfo>) { };
 
             virtual void beginRender() = 0;
             virtual void render() = 0;
             virtual void endRender() = 0;
 
             virtual ProgramList getPrograms() = 0;
+
+        protected:
+            friend class MapDrawingArea;
+
+            const MapDrawingArea* getOwningGLDrawingArea() const {
+                return m_owning_gl_drawing_area;
+            }
+
+        private:
+            const MapDrawingArea* m_owning_gl_drawing_area;
     };
 }
 
