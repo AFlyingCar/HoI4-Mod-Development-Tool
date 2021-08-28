fd412595998e70d64297d638311689681a872888
Tyler Robbins <tyler@familyrobbins.com>
2021-08-27 21:12:53 -0700

    Added the ability to render a special selection texture when a province is selected, to allow it to pop out more in comparison to the surrounding provinces.

--- map_normalizer/gui/gl/glsl/province_selection_fragment.glsl
+++ map_normalizer/gui/gl/glsl/province_selection_fragment.glsl
@@ -2,12 +2,14 @@
 
 out vec4 FragColor; // Output color value
 
-uniform sampler2D map_texture;
+uniform sampler2D selection_area;
+uniform sampler2D selection;
 
 in vec2 texture_coords; // Input from vertex shader
 
 void main() {
     // TODO: Do we want to colorize the selection? Maybe based on what the color of the province is?
-    FragColor = vec4(1.0, 0.0, 0.0, 1.0) * texture(map_texture, texture_coords);
+
+    FragColor = vec4(1.0, 0.0, 0.0, 1.0) * texture(selection, texture_coords) * vec4(1, 1, 1, texture(selection_area, texture_coords).a);
 }
 
