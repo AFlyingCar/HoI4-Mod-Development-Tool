f349b87ddb6729b691ce6401723f30ed2776adce
Tyler Robbins <tyler@familyrobbins.com>
2021-08-27 21:08:13 -0700

    Added Gui unit tests. Made gui a dependency of unit_tests.

---
+++ tests/inc/GuiTests.h
@@ -0,0 +1,23 @@
+
+#ifndef GUI_TESTS_H
+# define GUI_TESTS_H
+
+# include "gtest/gtest.h"
+
+# include "glibmm/refptr.h"
+# include "giomm/resource.h"
+# include "gtkmm.h"
+
+namespace MapNormalizer::UnitTests {
+    class GuiTests: public ::testing::Test {
+        protected:
+            void SetUp() override;
+            void TearDown() override;
+
+            Glib::RefPtr<Gio::Resource> m_resources;
+            Glib::RefPtr<Gtk::Application> m_app;
+    };
+}
+
+#endif
+
