79b36df23658029caaf1294e3ce57e16bbd26fe1
Tyler Robbins <tyler@familyrobbins.com>
2022-02-25 19:11:18 -0800

    Changed shader uniform() calls to pass textures directly rather than assuming the unit ID. Changed selection texture to use LINEAR MIN/MAG filters. Overhauled the province selection shader to make use of the label matrix on whether it should render the selection texture or not. Changed where the selection shader gets rendered to cover the entire map, rather than trying to render inside just the individual province.

--- map_normalizer/gui/gl/glsl/province_selection_fragment.glsl
+++ map_normalizer/gui/gl/glsl/province_selection_fragment.glsl
@@ -2,14 +2,21 @@
 
 out vec4 FragColor; // Output color value
 
-uniform sampler2D selection_area;
 uniform sampler2D selection;
+uniform usampler2D label_matrix;
+
+uniform uint province_label;
 
 in vec2 texture_coords; // Input from vertex shader
 
 void main() {
-    vec4 sel_color1 = texture(selection, texture_coords);
+    vec4 sel_color1 = texture(selection, texture_coords * 16); // TODO: This should either be a constant, or passed in via uniform
 
-    FragColor = vec4(1, 0, 0, texture(selection_area, texture_coords).a) * sel_color1;
+    uint pixel_label = texture(label_matrix, texture_coords).r;
+
+    // Only draw if the label for this fragment/pixel matches the one that is currently selected
+    float alpha = uint(pixel_label == province_label);
+
+    FragColor = sel_color1 * vec4(1, 0, 0, alpha);
 }
 
