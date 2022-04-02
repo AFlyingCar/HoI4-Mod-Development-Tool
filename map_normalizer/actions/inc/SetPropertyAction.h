8057cf91e0dde659b99b4d3b36015f9e282c08ab
Tyler Robbins <tyler@familyrobbins.com>
2022-04-02 00:00:28 -0700

    Added code to track the name of the field being set for debugging purposes.

--- map_normalizer/actions/inc/SetPropertyAction.h
+++ map_normalizer/actions/inc/SetPropertyAction.h
@@ -2,6 +2,10 @@
 #ifndef SETPROPERTYACTION_H
 # define SETPROPERTYACTION_H
 
+# include <string>
+
+# include "PreprocessorUtils.h"
+
 # include "Logger.h"
 
 # include "IAction.h"
@@ -18,18 +22,23 @@ namespace MapNormalizer::Action {
         public:
             SetPropertyAction(S* structure,
                               T S::* field,
-                              const T& new_value):
+                              const T& new_value,
+                              std::string_view field_name = ""):
                 m_structure(structure),
                 m_field(field),
                 m_old_value(structure->*m_field),
-                m_new_value(new_value)
+                m_new_value(new_value),
+                m_field_name(field_name)
             {
             }
 
             template<typename V,
                      typename = std::enable_if_t<std::is_convertible_v<V, T>>>
-            SetPropertyAction(S* structure, T S::* field, const V& new_value):
-                SetPropertyAction(structure, field, static_cast<const T&>(new_value))
+            SetPropertyAction(S* structure, T S::* field, const V& new_value,
+                              std::string_view field_name = ""):
+                SetPropertyAction(structure, field,
+                                  static_cast<const T&>(new_value),
+                                  field_name)
             { }
 
             virtual bool doAction(const Action::IAction::Callback& callback = _) override
@@ -41,7 +50,7 @@ namespace MapNormalizer::Action {
                     WRITE_ERROR("Current value does not match the old value!");
                     return false;
                 }
-                WRITE_DEBUG("Setting field.");
+                WRITE_DEBUG("Setting field '", m_field_name.data(), "'.");
                 (m_structure->*m_field) = m_new_value;
 
                 // Set back if we are told of a failure
@@ -62,7 +71,7 @@ namespace MapNormalizer::Action {
                     WRITE_ERROR("Current value does not match the old value!");
                     return false;
                 }
-                WRITE_DEBUG("UnSetting field.");
+                WRITE_DEBUG("UnSetting field '", m_field_name.data(), "'.");
                 (m_structure->*m_field) = m_old_value;
 
                 // Set back if we are told of a failure
@@ -86,13 +95,16 @@ namespace MapNormalizer::Action {
 
             //! The new value of the field
             T m_new_value;
+
+            //! A string representation of the field that is being set (for debug)
+            std::string_view m_field_name;
     };
 
 # define NewSetPropertyAction(OBJECT, NAME, VALUE) \
     new MapNormalizer::Action::SetPropertyAction< \
         MapNormalizer::RemoveAllPointers_t<decltype(OBJECT)>, \
                                            decltype(MapNormalizer::RemoveAllPointers_t<decltype(OBJECT)>::NAME)> \
-            (OBJECT, &MapNormalizer::RemoveAllPointers_t<decltype(OBJECT)>::NAME, VALUE)
+            (OBJECT, &MapNormalizer::RemoveAllPointers_t<decltype(OBJECT)>::NAME, VALUE, STR(NAME))
 }
 
 #endif
