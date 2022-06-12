f9d9a079253e388261ba2e73bda24c873043942f
Tyler Robbins <tyler@familyrobbins.com>
2022-06-11 22:42:43 -0700

    Added documentation.

--- mod_dev_tool/common/inc/StatusCodesDef.h
+++ mod_dev_tool/common/inc/StatusCodesDef.h
@@ -1,3 +1,13 @@
+/**
+ * @file StatusCodesDef.h
+ *
+ * @brief Defines every status code that will be used.
+ *
+ * @details To add new status codes, simply add a new entry to the
+ *          HMDT_STATUS_CODES x-macro. Make sure that no VALUE fields overlap,
+ *          as aliased status codes are _NOT_ supported.
+ */
+
 #ifndef STATUS_CODES_DEF_H
 # define STATUS_CODES_DEF_H
 
