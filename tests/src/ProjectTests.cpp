224b84ee86c6ad75f4f5f22c92c05256783d823c
Tyler Robbins <tyler@familyrobbins.com>
2023-04-23 14:03:45 -0700

    Added ProvinceProjectSaveAndLoad and ConvertV1ProvinceIDsToUUIDTest unit tests.

--- tests/src/ProjectTests.cpp
+++ tests/src/ProjectTests.cpp
@@ -2,15 +2,19 @@
 #include "gtest/gtest.h"
 
 #include <filesystem>
+#include <cstring>
+#include <fstream>
 
 #include "HoI4Project.h"
 #include "Constants.h"
 #include "StatusCodes.h"
 #include "Logger.h"
 #include "ShapeFinder2.h"
+#include "Util.h"
 
 #include "TestUtils.h"
 #include "TestMocks.h"
+#include "TestOverrides.h"
 
 TEST(ProjectTests, SimpleHoI4ProjectTest) {
     HMDT::Project::Project hproject;
@@ -354,6 +358,161 @@ TEST(ProjectTests, RiversProjectExportTemplateTest) {
 
     res = rivers_project.export_(export_path);
     ASSERT_SUCCEEDED(res);
+
+    HMDT::Log::Logger::getInstance().reset();
+}
+
+TEST(ProjectTests, ConvertV1ProvinceIDsToUUIDTest) {
+    SET_PROGRAM_OPTION(debug, true);
+
+    // We also want to see log outputs in the test output
+    HMDT::UnitTests::registerTestLogOutputFunction(true, true, true, true);
+
+    auto bin_path = HMDT::UnitTests::getTestProgramPath() / "bin";
+
+    // Load a project file that has a tool version number <= 0.25.0
+    auto project_path = HMDT::UnitTests::getTestProgramPath() / "bin" / "simple2.hoi4proj";
+    auto prov_path = HMDT::UnitTests::getTestProgramPath() / "bin" / "map_v1provinceids";
+    auto debug_path = HMDT::UnitTests::getTestProgramPath() / "bin" / "test_debug";
+
+    HMDT::UnitTests::HoI4ProjectMock hproject(project_path);
+    hproject.setDebugRoot(debug_path);
+
+    auto& map_project = hproject.getMapProject();
+    auto& prov_project = map_project.getProvinceProject();
+
+    // Make sure that we set up the MapData before attempting to load the provinces
+    {
+        // Hard-code these values since we aren't actually loading from a real
+        //   image here
+        auto iwidth = 512;
+        auto iheight = 512;
+
+        auto map_data = map_project.getMapData();
+
+        // Do a placement new so we keep the same memory location but update all
+        //  of the data inside the shared MapData instead, so that all
+        //  references are also updated too
+        map_data->~MapData();
+        new (map_data.get()) HMDT::MapData(iwidth, iheight);
+    }
+
+    ASSERT_STATUS(prov_project.load(prov_path), HMDT::STATUS_SUCCESS);
+
+    // TODO: Validate UUIDs are valid
+
+    HMDT::Log::Logger::getInstance().reset();
+}
+
+TEST(ProjectTests, ProvinceProjectSaveAndLoad) {
+    SET_PROGRAM_OPTION(debug, true);
+
+    // We also want to see log outputs in the test output
+    HMDT::UnitTests::registerTestLogOutputFunction(true, true, true, true);
+
+    auto bin_path = HMDT::UnitTests::getTestProgramPath() / "bin";
+
+    auto input_provinces_path = HMDT::UnitTests::getTestProgramPath() / "simple.bmp";
+
+    auto project_path = HMDT::UnitTests::getTestProgramPath() / "bin" / "simple2.hoi4proj";
+    auto prov_path = HMDT::UnitTests::getTestProgramPath() / "bin" / "map_province_saveandload";
+    auto debug_path = HMDT::UnitTests::getTestProgramPath() / "bin" / "test_debug";
+
+    HMDT::UnitTests::HoI4ProjectMock hproject(project_path);
+    hproject.setDebugRoot(debug_path);
+
+    auto& map_project = hproject.getMapProject();
+    auto& prov_project = map_project.getProvinceProject();
+
+    auto map_data = map_project.getMapData();
+
+    // Make sure that we set up the MapData before attempting to load the provinces
+    {
+        // Hard-code these values since we aren't actually loading from a real
+        //   image here
+        auto iwidth = 512;
+        auto iheight = 512;
+
+        // Do a placement new so we keep the same memory location but update all
+        //  of the data inside the shared MapData instead, so that all
+        //  references are also updated too
+        map_data->~MapData();
+        new (map_data.get()) HMDT::MapData(iwidth, iheight);
+    }
+
+    HMDT::MaybeVoid result = HMDT::STATUS_SUCCESS;
+
+    // First import test data
+    {
+        // TODO: ShapeFinder hasn't been switched over to the new BitMap2 object
+        //   yet, so we need to still use the deprecated one for now
+        WRITE_INFO("Loading ", input_provinces_path);
+        HMDT::BitMap* complex_bmp = HMDT::readBMP(input_provinces_path);
+        ASSERT_NE(complex_bmp, nullptr);
+
+        HMDT::ShapeFinder sf2(complex_bmp, HMDT::UnitTests::GraphicsWorkerMock::getInstance(), map_data);
+        sf2.findAllShapes();
+
+        prov_project.import(sf2, map_data);
+    }
+
+    std::filesystem::create_directories(prov_path);
+
+    // Make sure that the tool version is higher than 0.25.0
+    hproject.setToolVersion(HMDT::Version{"0.25.1"});
+
+    // Save the project
+    result = prov_project.save(prov_path);
+    ASSERT_SUCCEEDED(result);
+
+    // Save a copy of the map data here before loading so we can compare against
+    //   it
+    std::unique_ptr<HMDT::UUID[]> prov_data(new HMDT::UUID[map_data->getProvincesSize()]{ HMDT::EMPTY_UUID });
+    std::memcpy(prov_data.get(),
+                map_data->getProvinces().lock().get(),
+                map_data->getProvincesSize() * sizeof(HMDT::UUID));
+
+    // Load in the saved province data manually and verify that it matches what
+    //   we initially saved
+    WRITE_INFO("Reading saved province data from ",
+               prov_path / HMDT::SHAPEDATA_FILENAME);
+    if(std::ifstream in(prov_path / HMDT::SHAPEDATA_FILENAME); in) {
+        std::unique_ptr<HMDT::UUID[]> temp_data(new HMDT::UUID[map_data->getProvincesSize()]{ HMDT::EMPTY_UUID });
+
+        unsigned char magic[4];
+        uint32_t width = 0;
+        uint32_t height = 0;
+
+        // Read in the header first
+        ASSERT_TRUE(HMDT::safeRead(in, &magic, &width, &height));
+
+        // Verify that the magic bytes are correct
+        ASSERT_TRUE(HMDT::UnitTests::dynamicArraysMatch(
+                    reinterpret_cast<char*>(magic),
+                    HMDT::SHAPEDATA_MAGIC.data(), 4));
+
+        // Verify that the dimensions match
+        ASSERT_EQ(width * height, map_data->getMatrixSize());
+
+        in.read(reinterpret_cast<char*>(temp_data.get()),
+                map_data->getProvincesSize() * sizeof(HMDT::UUID));
+
+        ASSERT_TRUE(HMDT::UnitTests::dynamicArraysMatch(prov_data.get(),
+                                                        temp_data.get(),
+                                                        map_data->getProvincesSize()));
+    } else {
+        WRITE_ERROR("Failed to load province data.");
+        ASSERT_TRUE(false);
+    }
+
+    // Now load the project back into memory
+    result = prov_project.load(prov_path);
+    ASSERT_SUCCEEDED(result);
+
+    // Verify that the loaded province data matchces what was saved to disk
+    ASSERT_TRUE(std::equal(prov_data.get(),
+                           prov_data.get() + map_data->getProvincesSize(),
+                           map_data->getProvinces().lock().get()));
 
     HMDT::Log::Logger::getInstance().reset();
 }
